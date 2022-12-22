525c1e714ef6e341bc2188792cd003ca807e3d09
zdh <denizh@desird.com>
2022-12-22 17:23:01 +0300

    speedup second port.

--- speed_up_gsm.py
+++ speed_up_gsm.py
@@ -30,3 +30,32 @@ cmd = 'AT\r\n'
 port2.write(cmd.encode())
 rcv = port2.read(10)
 print (rcv)
+
+# Enable Serial Communication
+port3 = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=1)
+
+# Transmitting AT Commands to the Modem
+# '\r\n' indicates the Enter key
+
+cmd = "AT\r\n"
+port3.write(cmd.encode())
+rcv = port3.read(10)
+print (rcv)
+
+cmd = 'AT+IPR=460800\r\n'
+port3.write(cmd.encode())
+time.sleep(1)
+port3.close()
+
+port4 = serial.Serial("/dev/ttyAMA0", baudrate=460800, timeout=1)
+time.sleep(1)
+
+cmd = 'AT&W\r'
+port4.write(cmd.encode())
+rcv = port4.read(10)
+print (rcv)
+
+cmd = 'AT\r\n'
+port4.write(cmd.encode())
+rcv = port4.read(10)
+print (rcv)
